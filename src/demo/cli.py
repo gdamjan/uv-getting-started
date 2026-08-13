3564aa2dc3fb70e72ac61bcd2573a316ae4d2a6c
Дамјан Георгиевски <gdamjan@gmail.com>
2026-08-13 21:10:25 +0200

    in theory __package__ can be None
    for ex. if we run the script directly:
    `uv run src/demo/cli.py`
    
    so add a guard, metadata.version can't work without the __package__
    (well hard-coding the package name is ugly)
    
    better would be:
    `uv run -m demo.cli`
    
    or
    `uv run demo-cli`

--- src/demo/cli.py
+++ src/demo/cli.py
@@ -12,6 +12,12 @@ def main():
     parser.add_argument("-c", "--count")  # option that takes a value
     parser.add_argument("-v", "--verbose", action="store_true")  # on/off flag
 
-    print(f"Project version is: {importlib.metadata.version(__package__)}")
+    if __package__:
+        print(f"Project version is: {importlib.metadata.version(__package__)}")
+
     args = parser.parse_args()
     print(f"CLI args: {args!r}")
+
+
+if __name__ == "__main__":
+    main()
